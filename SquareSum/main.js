6653e6ff5fac92f6d52d47028103599dc63155e1
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-02-18 17:00:45 +0100

    square sum two ways done

--- SquareSum/main.js
+++ SquareSum/main.js
@@ -1,4 +1,6 @@
-function squareSum(numbers) {
+//WAY 1
+
+function squareSumVerboseWay(numbers) {
     if(Array.isArray(numbers)){
     let i = 0;
     let result = 0;
@@ -13,6 +15,16 @@ function squareSum(numbers) {
     }
 }
 
-console.log(squareSum([1, 2, 2])); //Expected result: 9
-console.log(squareSum([2, 2, 2])); //Expected result: 12
-console.log(squareSum([3, 1, 2, 3])); //Expected result: 23
+console.log(squareSumVerboseWay([1, 2, 2])); //Expected result: 9
+console.log(squareSumVerboseWay([2, 2, 2])); //Expected result: 12
+console.log(squareSumVerboseWay([3, 1, 2, 3])); //Expected result: 23
+
+//WAY 2
+
+function squareSumElegantWay(numbers) {
+  return numbers.reduce((acc, num) => acc + num*num, 0);
+}
+
+console.log(squareSumElegantWay([1, 2, 2])); //Expected result: 9
+console.log(squareSumElegantWay([2, 2, 2])); //Expected result: 12
+console.log(squareSumElegantWay([3, 1, 2, 3])); //Expected result: 23
